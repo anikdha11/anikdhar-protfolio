bfe66f28f039430b91d636f3fc76f2541cea4f70
anikdhar11 <aniksutradhar11@gmail.com>
2021-12-02 10:39:32 +0600

    intial commit

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 
@@ -11,17 +11,16 @@ const ProjectDetails = () => {
 
     useEffect(() => {
         fetch(
-           "https://anikdha11.github.io/api/db.json"
+         "https://my-json-server.typicode.com/anikdha11/api-all/projects"
         )
             .then((res) => res.json())
             .then((data) => {
                 const findData = data.find((project) => project.id == parseInt(detailsId));
                 if (findData) {
-                    console.log(findData)
                     setProjects(findData);
                 }
             });
-    }, []);
+    }, [detailsId]);
 
 
     return (
@@ -29,12 +28,20 @@ const ProjectDetails = () => {
             <h1>Projects {detailsId}</h1>
     
             <Container>
-                <Row className="p-5 mt-5 mb-5">
+                <Row className="p-5 mt-5 mb-5 mx-auto">
                     <Col md={6} sm={8}><img className="img-fluid" src={projects.img} alt="" /></Col>
                     <Col>
                     <h1>{projects.name}</h1>
                     <p>{projects.description}</p>
-                    
+                <div className="d-flex justify-content-evenly">
+                <Button variant="outline-dark"><a style={{textDecoration:"none",fontWeight:"900"}} href={projects.live}>Live-Site</a></Button>
+                {projects.client && <Button variant="outline-dark"><a style={{textDecoration:"none",fontWeight:"900"}} href={projects.client}>Client-code</a></Button>}
+            {projects.server && <Button variant="outline-dark"><a style={{textDecoration:"none",fontWeight:"900"}} href={projects.server}>Server-code</a></Button>}
+                </div>
+                   <br />
+                   <span>Technologies:</span> 
+                   <p>{projects.technologies}</p>
+                
                     </Col>
                 </Row>
              
