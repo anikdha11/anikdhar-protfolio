ec729f6fe657cf0054b37fc2b96c636819591581
Anik Dhar <aniksutradhar11@gmail.com>
2022-03-18 17:24:07 +0600

    header set

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -11,9 +11,8 @@ const Header = () => {
         <>
             <Navbar collapseOnSelect expand="lg" variant="light" sticky='top'>
                 <Container>
-                    <Navbar.Brand id="nav-link" as={HashLink} className="text-dark p-2" to="/home">Anik</Navbar.Brand>
                     <Navbar.Brand id="nav-link" as={HashLink} className="text-dark p-2" to="/home"><img className='icon' src="https://img.icons8.com/windows/32/000000/user-male.png" alt="" /></Navbar.Brand>
-                    <Navbar.Brand id="nav-link" as={HashLink} className="text-dark p-2" to="/home">Dhar</Navbar.Brand>
+                    <Navbar.Brand id="nav-link" as={HashLink} className="text-dark p-2" to="/home">Anik Dhar</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto"></Nav>
