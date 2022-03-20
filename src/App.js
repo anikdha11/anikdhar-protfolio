7fa677dcf75f1ac9359f726e4f5baf7ff2b0e5af
Anik Dhar <aniksutradhar11@gmail.com>
2022-03-20 17:20:57 +0600

    scroll added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Header from './Pages/Header/Header';
 import Footer from './Pages/Footer/Footer';
 import Blog from './Pages/Blog/Blog';
 import About from './Pages/About/About';
+import ScrollToTop from "react-scroll-to-top";
+import { SiAcclaim } from "react-icons/si"
 // import { SpinnerRoundFilled } from 'spinners-react';
 // import { useEffect, useState } from 'react';
 
@@ -23,15 +25,16 @@ function App() {
   //     setIsLoading(false);
   //   }, 500)
   // }, [])
+
+  
+
   return (
     <div className="App area">
-
       {/* {
         isLoading === true ? <SpinnerRoundFilled size={150} thickness={48} speed={71} color="rgba(137, 57, 172, 1)" className="mt-5 mx-auto" /> : another-link} */}
           <Router>
-            <Header></Header>,
-
-
+          <ScrollToTop style={{backgroundColor:"black"}} smooth component={<h5 style={{fontFamily:'sans-serif'}} className='fw-bold text-danger'>Up</h5>}/>
+            <Header></Header>
             <Switch>
               <Route exact path="/">
                 <Home></Home>
@@ -49,6 +52,7 @@ function App() {
                 <About></About>
               </Route>
             </Switch>
+          
             <Footer></Footer>
           </Router>
 
